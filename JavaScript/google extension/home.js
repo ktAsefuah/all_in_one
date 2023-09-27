ec873b3e703fb6851197f72caa771f6fa35c83ae
Kwesi <asefuahkt96@gmail.com>
2023-09-27 14:07:08 +0000

    update

--- JavaScript/google extension/home.js
+++ JavaScript/google extension/home.js
@@ -4,16 +4,31 @@ const ulEL = document.getElementById("ul-el")
 
 
 const inputBtn = document.getElementById("input-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    showLeads()
+}
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEL.value)
     inputEL.value =""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+   
+    console.log( localStorage.getItem("myLeads"))
     showLeads()
 })
+
 
 function showLeads(){
     let listItems = ""
     for (let i = 0; i < myLeads.length; i ++){
-        listItems += "<li><a href='#'>" + myLeads[i] + "</a></li>" 
+        listItems += `
+        <li>
+        <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}  </a>
+        </li>`
 
 }
 ulEL.innerHTML = listItems
@@ -24,4 +39,5 @@ ulEL.innerHTML = listItems
 const delAll = document.getElementById("del")
 delAll.addEventListener("click", function(){
     console.log("Button clicked")
+    localStorage.clear
 })
