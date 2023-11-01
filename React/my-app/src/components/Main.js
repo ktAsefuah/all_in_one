d3e251125caf577cedf5a862c48197939d2a54ca
Kwesi <asefuahkt96@gmail.com>
2023-11-01 14:54:05 +0000

    new updates

---
+++ React/my-app/src/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Main(){
+   return (
+    
+   <main>
+    <h1 className="main-title">Fun facts about React</h1>
+   
+    <ul className="facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile aps </li>
+    </ul>
+   </main>
+  
+   )  
+}
---
+++ React/my-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+ import React from "react"
+
+ export default function Navbar(){
+    return(
+        <nav>
+        <img src="logo192.png" alt="logo" className="icon"/>
+
+        <h3 className="nav-title">ReactFacts</h3>
+        <h4 className="nav-text">React Course - Project 1 </h4>
+        </nav>
+    ) 
+ }
